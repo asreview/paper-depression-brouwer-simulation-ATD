5c7cc09118433aac5ac594e8d3b476e09f3df5c9
JT <j.j.teijema@gmail.com>
2021-12-09 16:25:43 +0100

    Update runs

--- scripts/run_simulation.sh
+++ scripts/run_simulation.sh
@@ -6,6 +6,6 @@ SEED=$5
 
 # run a simulation
 rm output/${DATA}/simulation/run_${DATA}_final_${RUN}.h5
-asreview simulate data/${DATA}.csv -s output/${DATA}/simulation/run_${DATA}_${RUN}.h5 --prior_idx ${PRIOR_INCL} ${PRIOR_EXCL} --seed $SEED
+asreview simulate data/${DATA}.csv -s output/${DATA}/simulation/run_${DATA}_${RUN}.h5 --prior_idx ${PRIOR_INCL} ${PRIOR_EXCL} --seed $SEED --n_queries min
 python scripts/flatten_state.py output/${DATA}/simulation/run_${DATA}_${RUN}.h5 output/${DATA}/simulation/run_${DATA}_${RUN}_flat.csv
 rm output/${DATA}/simulation/run_${DATA}_${RUN}.h5
